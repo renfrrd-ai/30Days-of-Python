c03823198fabb8a93f6eb0192f3faa060068596a
renfrrd.ai <alongerenfred@gmail.com>
2025-11-26 02:45:08 -0500

    Day5

---
+++ day5/exercise1.py
@@ -0,0 +1,29 @@
+lst = []
+lst1 = [1, 2, 3, 4, 5, 6, 7]
+print(len(lst1))
+print(lst1[0], lst1[3], lst1[-1])
+mixed_data_types = ['Renfred', 17, '178cm', 'single', 'Mississauga']
+it_companies = ['Facebook', 'Google', 'Microsoft',
+                'Apple', 'IBM', 'Oracle', 'Amazon']
+print(it_companies)
+print(len(it_companies))
+print(it_companies[0], it_companies[3], it_companies[-1])
+it_companies[0] = 'Meta'
+print(it_companies)
+it_companies.append('Tesla')
+it_companies.insert(5, 'Nvidia')
+it_companies[3] = it_companies[3].upper()
+print('#; '.join(it_companies))
+print('Google' in it_companies)
+print(it_companies[:3])
+print(it_companies[len(it_companies)-3:])
+print(it_companies[3:4])
+del it_companies[0], it_companies[3], it_companies[-1]
+it_companies.clear()
+del it_companies
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node', 'Express', 'MongoDB']
+front_end.extend(back_end)
+full_stack = front_end.copy()
+full_stack.insert(5, Python)
+full_stack.insert(6, SQL)
---
+++ day5/exercise2.py
@@ -0,0 +1,12 @@
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages[0], ages[-1])
+# ages.append(ages[0]).append(ages[-1])
+median_age = (ages[4]+ages[5])/2
+total_age = 0
+for age in ages:
+    total_age += age
+avg = total_age/len(age)
+range = ages[-1] - ages[0]
+print(avg)
+print(abs(ages[0] - avg) == abs(ages[-1] - avg))
